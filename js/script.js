bbf115521c4865b38da36f26b07d2954f1e6e48f
davide vanoncini <davidevanoncini2003@gmail.com>
2021-11-25 03:31:55 +0100

    almost finished the choice interface area

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function toggleShow(id) {
+    if (document.getElementById(id).style.display == 'block') {
+        document.getElementById(id).style.display = 'none';
+    } else {
+        document.getElementById(id).style.display = 'block';
+    }
+}
+
+function show(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
+function hide(id) {
+    document.getElementById(id).style.display = 'none';
+}
