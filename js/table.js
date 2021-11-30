4d8ad9dd904c901192287ced8389cb4a11b2cdf5
davide vanoncini <davidevanoncini2003@gmail.com>
2021-11-30 21:27:40 +0100

    removed debug

--- js/table.js
+++ js/table.js
@@ -40,41 +40,30 @@ function generateTable(cols, rows) {
     var tbody = document.createElement("tbody");
     for (var r = 0; r < rows; r++) {
         var row = document.createElement("tr");
-        // console.log("i: ", r);
         row.setAttribute("id", "row" + r);
         //create a table cell for each column in the table
         for (var c = 0; c < cols + 2; c++) {
             var cell = document.createElement("td");
             cell.setAttribute("scope", "col");
-            // console.log("c: ", c);
             if (c == 0) {
                 cell.innerHTML = "UP" + parseInt(r + 1).toString();
-                // console.log(cell)
-                // console.log("sono nel for, up:", c)
             } else if (c == cols + 1) {
-                // cell.innerHTML = "Totali";
                 cell.setAttribute("id", "cell" + r + "" + c);
                 cell.setAttribute("class", "cell");
                 cell.innerHTML = "";
-                // console.log("sono nel for, totali:", c)
             }
             else {
-                // console.log("sono nell if incrimitanto:", c)
                 cell.setAttribute("id", "cell" + r + "" + c);
                 cell.setAttribute("class", "cell");
                 cell.setAttribute("contenteditable", "true");
                 cell.innerHTML = "";
                 //add the cell to the row
             }
-            // console.log("cella num " + c + ": ", cell);
             row.appendChild(cell);
-            // console.log("row " + c + ": ", row);
         }
         //add the row to the table
         tbody.appendChild(row);
-        // console.log(tbody);
     }
     table.appendChild(tbody);
-    // console.log(table);
     document.getElementById("tableContainer").replaceChild(table, document.getElementById("tableContainer").childNodes[0]);
 }
