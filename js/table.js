44050f0c3c2d21548c45f1621eea54f28c433fd9
davide vanoncini <davidevanoncini2003@gmail.com>
2021-11-30 21:26:58 +0100

    generate table works

--- js/table.js
+++ js/table.js
@@ -1,11 +1,14 @@
+"use strict";
+
 function generateTable(cols, rows) {
     cols = parseInt(cols);
     rows = parseInt(rows);
 
     //create an editable table with the given height and width
     var table = document.createElement("table");
+
     table.setAttribute("id", "table");
-    table.setAttribute("class", "table");
+    table.setAttribute("class", "table table-striped table-bordered");
     table.setAttribute("border", "1");
     table.setAttribute("align", "center");
 
@@ -16,7 +19,6 @@ function generateTable(cols, rows) {
     for (var i = 0; i < cols + 1; i++) {
         var cell = document.createElement("td");
 
-        console.log("sono nel for del head:", i)
         cell.setAttribute("scope", "col")
         if (i == 0) {
             cell.innerHTML = "#";
@@ -24,42 +26,55 @@ function generateTable(cols, rows) {
             cell.innerHTML = "D " + (i);
         }
         row.appendChild(cell);
-        console.log(row);
         thead.appendChild(row);
     }
+
     var cell = document.createElement("td");
-
-
-    console.log("fine for")
     cell.setAttribute("scope", "col")
     cell.innerHTML = "Totali";
     row.appendChild(cell);
     thead.appendChild(row);
-    table.appendChild(thead);
-    console.log("sono qui")
 
-    // var tbody = document.createElement("tbody");
-    // for (var i = 0; i < cols + 2; i++) {
-    //     row.setAttribute("id", "row" + i);
-    //     row.setAttribute("class", "row");
-    //     //create a table cell for each column in the table
-    //     for (var j = 0; j < rows + 2; j++) {
-    //         if (j == 0) {
-    //             cell.innerHTML = "UP" + j;
-    //         } else if (j == rows + 2) {
-    //             cell.innerHTML = "Totali";
-    //         } else {
-    //             cell.setAttribute("id", "cell" + i + j);
-    //             cell.setAttribute("class", "cell");
-    //             cell.setAttribute("contenteditable", "true");
-    //             cell.innerHTML = "";
-    //             //add the cell to the row
-    //             row.appendChild(cell);
-    //         }
-    //     }
-    //     //add the row to the table
-    //     tbody.appendChild(row);
-    // }
-    // table.appendChild(tbody);
-    document.getElementById("tableContainer").appendChild(table);
+    table.appendChild(thead);
+
+    var tbody = document.createElement("tbody");
+    for (var r = 0; r < rows; r++) {
+        var row = document.createElement("tr");
+        // console.log("i: ", r);
+        row.setAttribute("id", "row" + r);
+        //create a table cell for each column in the table
+        for (var c = 0; c < cols + 2; c++) {
+            var cell = document.createElement("td");
+            cell.setAttribute("scope", "col");
+            // console.log("c: ", c);
+            if (c == 0) {
+                cell.innerHTML = "UP" + parseInt(r + 1).toString();
+                // console.log(cell)
+                // console.log("sono nel for, up:", c)
+            } else if (c == cols + 1) {
+                // cell.innerHTML = "Totali";
+                cell.setAttribute("id", "cell" + r + "" + c);
+                cell.setAttribute("class", "cell");
+                cell.innerHTML = "";
+                // console.log("sono nel for, totali:", c)
+            }
+            else {
+                // console.log("sono nell if incrimitanto:", c)
+                cell.setAttribute("id", "cell" + r + "" + c);
+                cell.setAttribute("class", "cell");
+                cell.setAttribute("contenteditable", "true");
+                cell.innerHTML = "";
+                //add the cell to the row
+            }
+            // console.log("cella num " + c + ": ", cell);
+            row.appendChild(cell);
+            // console.log("row " + c + ": ", row);
+        }
+        //add the row to the table
+        tbody.appendChild(row);
+        // console.log(tbody);
+    }
+    table.appendChild(tbody);
+    // console.log(table);
+    document.getElementById("tableContainer").replaceChild(table, document.getElementById("tableContainer").childNodes[0]);
 }
